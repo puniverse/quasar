7d11e13fbc403180b42566a07a40c0cdbfb8cd3f
pron <ron.pressler@gmail.com>
2013-04-03 22:04:55 +0300

    added interrupt test

--- src/test/java/co/paralleluniverse/lwthreads/LightweightThreadTest.java
+++ src/test/java/co/paralleluniverse/lwthreads/LightweightThreadTest.java
@@ -4,6 +4,7 @@
  */
 package co.paralleluniverse.lwthreads;
 
+import co.paralleluniverse.common.util.Exceptions;
 import java.util.concurrent.TimeUnit;
 import jsr166e.ForkJoinPool;
 import org.junit.After;
@@ -26,6 +27,12 @@ public class LightweightThreadTest {
 
     @BeforeClass
     public static void setUpClass() {
+        LightweightThread.setDefaultUncaughtExceptionHandler(new LightweightThread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(LightweightThread lwt, Throwable e) {
+                Exceptions.rethrow(e);
+            }
+        });
     }
 
     @AfterClass
@@ -47,15 +54,33 @@ public class LightweightThreadTest {
             public void run() throws SuspendExecution {
                 LightweightThread.park(100, TimeUnit.MILLISECONDS);
             }
-        });
-        lwt.start();
+        }).start();
 
+        
         try {
             lwt.join(2, TimeUnit.MILLISECONDS);
             fail();
         } catch (java.util.concurrent.TimeoutException e) {
         }
-        
+
         lwt.join(200, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void testInterrupt() throws Exception {
+        LightweightThread lwt = new LightweightThread(fjPool, new SuspendableRunnable() {
+            @Override
+            public void run() throws SuspendExecution {
+                try {
+                    LightweightThread.sleep(100);
+                    fail("InterruptedException not thrown");
+                } catch (LwtInterruptedException e) {
+                }
+            }
+        });
+        lwt.start();
+
+        Thread.sleep(20);
+        lwt.interrupt();
     }
 }
