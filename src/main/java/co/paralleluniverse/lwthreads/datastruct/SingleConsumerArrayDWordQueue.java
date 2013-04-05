b483daf9edd78e89c4ad171a159f9a3ff94466c2
pron <ron.pressler@gmail.com>
2013-04-05 23:40:19 +0300

    use regular array read/write semantics for SCArrayWordQ and SCArrayDWordQ

--- src/main/java/co/paralleluniverse/lwthreads/datastruct/SingleConsumerArrayDWordQueue.java
+++ src/main/java/co/paralleluniverse/lwthreads/datastruct/SingleConsumerArrayDWordQueue.java
@@ -26,38 +26,12 @@ abstract class SingleConsumerArrayDWordQueue<E> extends SingleConsumerArrayPrimi
 
     void enq(long item) {
         final int i = preEnq();
-        set(i, item);
+        array[i] = item; // no need for volatile semantics because postEnq does a volatile write (cas) which is then read in await value
         postEnq(i);
     }
 
     @Override
     void copyValue(int to, int from) {
-        lazySet(to, array[from]);
-    }
-    private static final int base;
-    private static final int shift;
-
-    static {
-        try {
-            base = unsafe.arrayBaseOffset(long[].class);
-            int scale = unsafe.arrayIndexScale(long[].class);
-            if ((scale & (scale - 1)) != 0)
-                throw new Error("data type scale not a power of two");
-            shift = 31 - Integer.numberOfLeadingZeros(scale);
-        } catch (Exception ex) {
-            throw new Error(ex);
-        }
-    }
-
-    private static long byteOffset(int i) {
-        return ((long) i << shift) + base;
-    }
-
-    private void set(int i, long value) {
-        unsafe.putLongVolatile(array, byteOffset(i), value);
-    }
-
-    private void lazySet(int i, long value) {
-        unsafe.putOrderedLong(array, byteOffset(i), value);
+        array[to] = array[from];
     }
 }
--- src/main/java/co/paralleluniverse/lwthreads/datastruct/SingleConsumerArrayWordQueue.java
+++ src/main/java/co/paralleluniverse/lwthreads/datastruct/SingleConsumerArrayWordQueue.java
@@ -26,38 +26,12 @@ abstract class SingleConsumerArrayWordQueue<E> extends SingleConsumerArrayPrimit
 
     void enq(int item) {
         final int i = preEnq();
-        set(i, item);
+        array[i] = item; // no need for volatile semantics because postEnq does a volatile write (cas) which is then read in await value
         postEnq(i);
     }
 
     @Override
     void copyValue(int to, int from) {
-        lazySet(to, array[from]);
-    }
-    private static final int base;
-    private static final int shift;
-
-    static {
-        try {
-            base = unsafe.arrayBaseOffset(int[].class);
-            int scale = unsafe.arrayIndexScale(int[].class);
-            if ((scale & (scale - 1)) != 0)
-                throw new Error("data type scale not a power of two");
-            shift = 31 - Integer.numberOfLeadingZeros(scale);
-        } catch (Exception ex) {
-            throw new Error(ex);
-        }
-    }
-
-    private static long byteOffset(int i) {
-        return ((long) i << shift) + base;
-    }
-
-    private void set(int i, int value) {
-        unsafe.putIntVolatile(array, byteOffset(i), value);
-    }
-
-    private void lazySet(int i, int value) {
-        unsafe.putOrderedInt(array, byteOffset(i), value);
+        array[to] = array[from];
     }
 }
