e7a729571d4aa9840757dcdeee11a193b696bc92
pron <ron.pressler@gmail.com>
2013-05-24 15:00:24 +0300

    Created channels.Topic

---
+++ src/main/java/co/paralleluniverse/strands/channels/Topic.java
@@ -0,0 +1,43 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (C) 2013, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.strands.channels;
+
+import java.util.Collection;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ *
+ * @author pron
+ */
+public class Topic<Message> implements SendChannel<Message> {
+    private final Collection<SendChannel<? super Message>> subscribers;
+
+    public Topic() {
+        this.subscribers = new CopyOnWriteArraySet<>();
+    }
+
+    public void subscribe(SendChannel<? super Message> sub) {
+        subscribers.add(sub);
+    }
+
+    public void unsubscribe(SendChannel<? super Message> sub) {
+        subscribers.remove(sub);
+    }
+
+    @Override
+    public void send(Message message) {
+        for (SendChannel<? super Message> sub : subscribers)
+            sub.send(message);
+    }
+}
