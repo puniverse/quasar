805d0689fe6985eebc4ad6eb082b33fde6996bf1
pron <ron.pressler@gmail.com>
2013-05-28 12:25:20 +0300

    -

--- src/main/java/co/paralleluniverse/actors/ShutdownMessage.java
+++ src/main/java/co/paralleluniverse/actors/ShutdownMessage.java
@@ -11,7 +11,7 @@
  * under the terms of the GNU Lesser General Public License version 3.0
  * as published by the Free Software Foundation.
  */
-package co.paralleluniverse.actors.behaviors;
+package co.paralleluniverse.actors;
 
 import co.paralleluniverse.actors.*;
 import java.util.Objects;
