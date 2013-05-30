252e4e099707eacd7d58cac7ac01bc66683b066d
pron <ron.pressler@gmail.com>
2013-05-30 18:47:57 +0300

    added logging

---
+++ src/main/java/co/paralleluniverse/common/logging/LoggingUtils.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2011-2013, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.common.logging;
+
+/**
+ *
+ * @author pron
+ */
+public final class LoggingUtils {
+    public static String hex(long num) {
+        return Long.toHexString(num);
+    }
+    
+    public static String hex(int num) {
+        return Integer.toHexString(num);
+    }
+    
+    private LoggingUtils() {
+    }
+}
