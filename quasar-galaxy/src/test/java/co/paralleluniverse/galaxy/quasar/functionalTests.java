63d952f245c45ebc2bc0e049dea44eeda3a0e7c0
eitan <eitan@paralleluniverse.co>
2013-06-25 08:32:33 +0300

    galaxy config dir

---
+++ quasar-galaxy/src/test/java/co/paralleluniverse/galaxy/quasar/functionalTests.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.paralleluniverse.galaxy.quasar;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author eitan
+ */
+public class functionalTests {
+    public functionalTests() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        return;
+    }
+}
