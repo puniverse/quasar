97d66bccf4fc240173b1dd79ea0e84feea89be80
pron <ron.pressler@gmail.com>
2013-09-16 16:18:54 +0300

    Added Record.copy() and Records.deepEquals()

--- quasar-actors/src/main/java/co/paralleluniverse/data/record/Records.java
+++ quasar-actors/src/main/java/co/paralleluniverse/data/record/Records.java
@@ -12,6 +12,9 @@
  */
 package co.paralleluniverse.data.record;
 
+import java.util.Collection;
+import java.util.Objects;
+
 /**
  *
  * @author pron
@@ -22,9 +25,207 @@ public final class Records {
     }
 
     public static <R> void setDelegate(Record<R> record, Object owner, Record<R> newDelegate) {
-        if(!(record instanceof RecordDelegate))
+        if (!(record instanceof RecordDelegate))
             throw new UnsupportedOperationException("Record " + record + " is not a record delegate");
-        ((RecordDelegate<R>)record).setDelegate(owner, newDelegate);
+        ((RecordDelegate<R>) record).setDelegate(owner, newDelegate);
+    }
+
+    public static <R> void copy(Record<R> source, Record<R> target) {
+        copy(source, target, target.fields());
+    }
+
+    public static <R> void copy(Record<R> source, Record<R> target, Collection<Field<? super R, ?>> fields) {
+        for (Field<? super R, ?> field : fields) {
+            try {
+                switch (field.type()) {
+                    case Field.BOOLEAN:
+                        target.set((Field.BooleanField) field, source.get((Field.BooleanField) field));
+                        break;
+                    case Field.BYTE:
+                        target.set((Field.ByteField) field, source.get((Field.ByteField) field));
+                        break;
+                    case Field.SHORT:
+                        target.set((Field.ShortField) field, source.get((Field.ShortField) field));
+                        break;
+                    case Field.INT:
+                        target.set((Field.IntField) field, source.get((Field.IntField) field));
+                        break;
+                    case Field.LONG:
+                        target.set((Field.LongField) field, source.get((Field.LongField) field));
+                        break;
+                    case Field.FLOAT:
+                        target.set((Field.FloatField) field, source.get((Field.FloatField) field));
+                        break;
+                    case Field.DOUBLE:
+                        target.set((Field.DoubleField) field, source.get((Field.DoubleField) field));
+                        break;
+                    case Field.CHAR:
+                        target.set((Field.CharField) field, source.get((Field.CharField) field));
+                        break;
+                    case Field.OBJECT:
+                        target.set((Field.ObjectField) field, source.get((Field.ObjectField) field));
+                        break;
+                    case Field.BOOLEAN_ARRAY:
+                        target.set((Field.BooleanArrayField) field, source, (Field.BooleanArrayField) field);
+                        break;
+                    case Field.BYTE_ARRAY:
+                        target.set((Field.ByteArrayField) field, source, (Field.ByteArrayField) field);
+                        break;
+                    case Field.SHORT_ARRAY:
+                        target.set((Field.ShortArrayField) field, source, (Field.ShortArrayField) field);
+                        break;
+                    case Field.INT_ARRAY:
+                        target.set((Field.IntArrayField) field, source, (Field.IntArrayField) field);
+                        break;
+                    case Field.LONG_ARRAY:
+                        target.set((Field.LongArrayField) field, source, (Field.LongArrayField) field);
+                        break;
+                    case Field.FLOAT_ARRAY:
+                        target.set((Field.FloatArrayField) field, source, (Field.FloatArrayField) field);
+                        break;
+                    case Field.DOUBLE_ARRAY:
+                        target.set((Field.DoubleArrayField) field, source, (Field.DoubleArrayField) field);
+                        break;
+                    case Field.CHAR_ARRAY:
+                        target.set((Field.CharArrayField) field, source, (Field.CharArrayField) field);
+                        break;
+                    case Field.OBJECT_ARRAY:
+                        target.set((Field.ObjectArrayField) field, source, (Field.ObjectArrayField) field);
+                        break;
+                    default:
+                        throw new AssertionError();
+                }
+            } catch (FieldNotFoundException e) {
+            }
+        }
+    }
+
+    public static <R> boolean deepEquals(Record<R> a, Record<R> b) {
+        if (a == b)
+            return true;
+        if (a == null | b == null)
+            return false;
+        if (!a.fields().equals(b.fields()))
+            return false;
+        for (Field<? super R, ?> field : a.fields()) {
+            try {
+                switch (field.type()) {
+                    case Field.BOOLEAN:
+                        if (a.get((Field.BooleanField) field) != b.get((Field.BooleanField) field))
+                            return false;
+                        break;
+                    case Field.BYTE:
+                        if (a.get((Field.ByteField) field) != b.get((Field.ByteField) field))
+                            return false;
+                        break;
+                    case Field.SHORT:
+                        if (a.get((Field.ShortField) field) != b.get((Field.ShortField) field))
+                            return false;
+                        break;
+                    case Field.INT:
+                        if (a.get((Field.IntField) field) != b.get((Field.IntField) field))
+                            return false;
+                        break;
+                    case Field.LONG:
+                        if (a.get((Field.LongField) field) != b.get((Field.LongField) field))
+                            return false;
+                        break;
+                    case Field.FLOAT:
+                        if (a.get((Field.FloatField) field) != b.get((Field.FloatField) field))
+                            return false;
+                        break;
+                    case Field.DOUBLE:
+                        if (a.get((Field.DoubleField) field) != b.get((Field.DoubleField) field))
+                            return false;
+                        break;
+                    case Field.CHAR:
+                        if (a.get((Field.CharField) field) != b.get((Field.CharField) field))
+                            return false;
+                        break;
+                    case Field.OBJECT:
+                        if (!(Objects.equals(a.get((Field.ObjectField) field), b.get((Field.ObjectField) field))))
+                            return false;
+                        break;
+                    case Field.BOOLEAN_ARRAY: {
+                        Field.BooleanArrayField f = (Field.BooleanArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.BYTE_ARRAY: {
+                        Field.ByteArrayField f = (Field.ByteArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.SHORT_ARRAY: {
+                        Field.ShortArrayField f = (Field.ShortArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.INT_ARRAY: {
+                        Field.IntArrayField f = (Field.IntArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.LONG_ARRAY: {
+                        Field.LongArrayField f = (Field.LongArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.FLOAT_ARRAY: {
+                        Field.FloatArrayField f = (Field.FloatArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.DOUBLE_ARRAY: {
+                        Field.DoubleArrayField f = (Field.DoubleArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.CHAR_ARRAY: {
+                        Field.CharArrayField f = (Field.CharArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (a.get(f, i) != b.get(f, i))
+                                return false;
+                        }
+                        break;
+                    }
+                    case Field.OBJECT_ARRAY: {
+                        Field.ObjectArrayField f = (Field.ObjectArrayField) field;
+                        for (int i = 0; i < f.length; i++) {
+                            if (Objects.equals(a.get(f, i), b.get(f, i)))
+                                return false;
+                        }
+                        break;
+                    }
+                    default:
+                        throw new AssertionError();
+                }
+            } catch (FieldNotFoundException e) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private Records() {
