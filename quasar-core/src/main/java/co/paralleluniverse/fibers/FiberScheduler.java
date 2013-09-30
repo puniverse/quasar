a0583b1ecb9edcc11f7a2842f6383ea55ec2f895
pron <ron.pressler@gmail.com>
2013-09-30 18:45:21 +0300

    FiberScheduler imporvements

--- quasar-core/src/main/java/co/paralleluniverse/fibers/FiberScheduler.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/FiberScheduler.java
@@ -4,6 +4,13 @@
  */
 package co.paralleluniverse.fibers;
 
+import co.paralleluniverse.common.monitoring.ForkJoinPoolMonitor;
+import co.paralleluniverse.common.monitoring.JMXForkJoinPoolMonitor;
+import co.paralleluniverse.common.monitoring.MetricsForkJoinPoolMonitor;
+import co.paralleluniverse.common.monitoring.MonitorType;
+import co.paralleluniverse.concurrent.forkjoin.MonitoredForkJoinPool;
+import co.paralleluniverse.concurrent.util.NamingForkJoinWorkerFactory;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import jsr166e.ForkJoinPool;
 
 /**
@@ -18,13 +25,50 @@ public class FiberScheduler {
         this.fjPool = fjPool;
         this.timer = timeService;
     }
-    
+
     public FiberScheduler(ForkJoinPool fjPool) {
-        this.fjPool = fjPool;
-        this.timer = new FiberTimedScheduler(fjPool);
+        this(fjPool, createTimer(fjPool));
     }
 
-    ForkJoinPool getFjPool() {
+    public FiberScheduler(String name, int parallelism, MonitorType monitorType) {
+        this(createForkJoinPool(name, parallelism, monitorType));
+    }
+
+    private static ForkJoinPool createForkJoinPool(String name, int parallelism, MonitorType monitorType) {
+        final MonitoredForkJoinPool fjPool = new MonitoredForkJoinPool(name, parallelism, new NamingForkJoinWorkerFactory(name), null, true);
+        final ForkJoinPoolMonitor fjpMonitor;
+        final FibersMonitor fibersMonitor;
+        switch (monitorType) {
+            case JMX:
+                fjpMonitor = new JMXForkJoinPoolMonitor(name, fjPool);
+                fibersMonitor = new JMXFibersMonitor(name, fjPool);
+                break;
+            case METRICS:
+                fjpMonitor = new MetricsForkJoinPoolMonitor(name, fjPool);
+                fibersMonitor = new MetricsFibersMonitor(name, fjPool);
+                break;
+            case NONE:
+                fjpMonitor = null;
+                fibersMonitor = new NoopFibersMonitor();
+                break;
+            default:
+                throw new RuntimeException("Unsupported monitor type: " + monitorType);
+        }
+
+        fjPool.setMonitor(fjpMonitor);
+        fjPool.setFibersMonitor(fibersMonitor);
+        return fjPool;
+    }
+
+    private static FiberTimedScheduler createTimer(ForkJoinPool fjPool) {
+        if (fjPool instanceof MonitoredForkJoinPool)
+            return new FiberTimedScheduler(fjPool,
+                    new ThreadFactoryBuilder().setDaemon(true).setNameFormat(((MonitoredForkJoinPool) fjPool).getName()).build());
+        else
+            return new FiberTimedScheduler(fjPool);
+    }
+
+    public ForkJoinPool getFjPool() {
         return fjPool;
     }
 
