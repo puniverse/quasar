b9354192d2f7696f85be43c42e643477f2303cc1
pron <ron.pressler@gmail.com>
2013-11-05 11:43:15 +0200

    AnnotationUtil work

--- quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/AnnotationUtil.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/AnnotationUtil.java
@@ -41,6 +41,11 @@ public final class AnnotationUtil {
         final String annDesc = Type.getDescriptor(annClass);
         final AtomicBoolean res = new AtomicBoolean(false);
         r.accept(new ClassVisitor(Opcodes.ASM4) {
+
+            @Override
+            public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+            }
+            
             @Override
             public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
                 if (desc.equals(annDesc))
