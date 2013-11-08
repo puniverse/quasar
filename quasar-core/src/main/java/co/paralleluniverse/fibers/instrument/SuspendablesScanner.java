7eb223fb067008fc11a1cd693333333813f073f4
pron <ron.pressler@gmail.com>
2013-11-08 12:52:54 +0200

    Use ASMUtil instead of reflection in SuspendableScanner

--- quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/SuspendablesScanner.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/instrument/SuspendablesScanner.java
@@ -14,9 +14,13 @@
 package co.paralleluniverse.fibers.instrument;
 
 import co.paralleluniverse.fibers.Suspendable;
+import static co.paralleluniverse.fibers.instrument.ASMUtil.*;
 import static co.paralleluniverse.fibers.instrument.SimpleSuspendableClassifier.PREFIX;
 import static co.paralleluniverse.fibers.instrument.SimpleSuspendableClassifier.SUSPENDABLE_SUPERS_FILE;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
 import java.net.URL;
@@ -26,12 +30,17 @@ import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.AnnotationNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 /**
  *
  * @author pron
  */
 public class SuspendablesScanner {
+    private static final boolean USE_REFLECTION = false;
     private static final String BUILD_DIR = "build/";
     private static final String RESOURCES_DIR = "resources/main/";
     private static final String CLASSES_DIR = "/classes/main/";
@@ -58,7 +67,7 @@ public class SuspendablesScanner {
             URL url = urls.nextElement();
             File file = new File(url.getFile());
             if (file.isDirectory())
-                scanClasses(file, results);
+                scanClasses(file, results, ClassLoader.getSystemClassLoader());
         }
         return results;
     }
@@ -68,8 +77,8 @@ public class SuspendablesScanner {
             List<String> sorted = new ArrayList<String>(results);
             Collections.sort(sorted);
             for (String s : sorted) {
-    //            if(out != System.out)
-    //                System.out.println(s);
+                //            if(out != System.out)
+                //                System.out.println(s);
                 out.println(s);
             }
         }
@@ -91,15 +100,19 @@ public class SuspendablesScanner {
             return System.out;
     }
 
-    private static void scanClasses(File file, Set<String> results) throws Exception {
+    private static void scanClasses(File file, Set<String> results, ClassLoader cl) throws Exception {
         if (file.isDirectory()) {
             System.out.println("Scanning dir: " + file.getPath());
             for (File f : file.listFiles())
-                scanClasses(f, results);
+                scanClasses(f, results, cl);
         } else {
             String className = extractClassName(file);
-            if (className != null)
-                scanClass(Class.forName(className), results);
+            if (className != null) {
+                if (USE_REFLECTION)
+                    scanClass(Class.forName(className), results);
+                else
+                    scanClass(getClassNode(className, true, cl), results, cl);
+            }
         }
     }
 
@@ -111,6 +124,27 @@ public class SuspendablesScanner {
             return className;
         } else
             return null;
+    }
+
+    private static void scanClass(ClassNode cls, Set<String> results, ClassLoader cl) throws Exception {
+        List<MethodNode> methods = cls.methods;
+        for (MethodNode m : methods) {
+            if (hasAnnotation(Suspendable.class, m))
+                findSuperDeclarations(cls, cls, m, results, cl);
+        }
+    }
+
+    private static void findSuperDeclarations(ClassNode cls, ClassNode declaringClass, MethodNode method, Set<String> results, ClassLoader cl) throws IOException {
+        if (cls == null)
+            return;
+
+        if (!ASMUtil.equals(cls, declaringClass) && hasMethod(method, cls))
+            results.add(cls.name.replace('/', '.') + '.' + method.name);
+
+        // recursively look in superclass and interfaces
+        findSuperDeclarations(getClassNode(cls.superName, true, cl), declaringClass, method, results, cl);
+        for (String iface : (List<String>) cls.interfaces)
+            findSuperDeclarations(getClassNode(iface, true, cl), declaringClass, method, results, cl);
     }
 
     private static void scanClass(Class cls, Set<String> results) throws Exception {
