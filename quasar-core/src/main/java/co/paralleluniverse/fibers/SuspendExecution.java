2eed6b40e237b876847e58f468c214e5b7d07312
pron <ron.pressler@gmail.com>
2013-12-11 14:09:56 +0200

    -

--- quasar-core/src/main/java/co/paralleluniverse/fibers/SuspendExecution.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/SuspendExecution.java
@@ -1,53 +1,33 @@
 /*
- * Copyright (c) 2008, Matthias Mann
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (C) 2013, Parallel Universe Software Co. All rights reserved.
  * 
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Matthias Mann nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
  */
 package co.paralleluniverse.fibers;
 
 /**
- * <p>An exception used to initiate the control transfer.</p>
- * <p>It does not support stack traces.</p>
- * 
- * <p>Methods which are declared to throw this exception are "suspendable". This
- * exception must always be propagated and never be caught.</p>
- * 
- * <p>Generic try/catch handlers are allowed:<br/>
- * {@code try{ doSomething(); } catch(Throwable ex) { handleException(ex); } }</p>
- * 
- * <p>The instrumentation ANT task will enhance the bytecode of these methods to
- * support suspension and continuation of their execution.</p>
- * 
- * @author Matthias Mann
+ * An exception used to initiate the control transfer.
+ * Methods which are declared to throw this exception are "suspendable". This
+ * exception must always be propagated and never be caught.
+ * <p/>
+ * Try/catch handlers are allowed:<br/>
+ * {@code try{ doSomething(); } catch(Throwable ex) { handleException(ex); } }
+ * <p/>
+ * Quasar instrumentation will enhance the bytecode of these methods to
+ * support suspension and continuation of their execution; they will not catch {@code SuspendExecution}.
  */
 public final class SuspendExecution extends Exception { // InterruptedException {
-
     static final SuspendExecution PARK = new SuspendExecution();
     static final SuspendExecution YIELD = new SuspendExecution();
-    
+
     SuspendExecution() {
     }
 
@@ -55,5 +35,4 @@ public final class SuspendExecution extends Exception { // InterruptedException 
     public Throwable fillInStackTrace() {
         return this;
     }
-
 }
