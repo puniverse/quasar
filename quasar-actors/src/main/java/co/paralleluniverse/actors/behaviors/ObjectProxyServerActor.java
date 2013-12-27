f3d3515ab3fe0bff6d70a9569c7fa2266338cc06
pron <ron.pressler@gmail.com>
2013-12-27 19:08:05 +0200

    Add ObjectProxyServerActor + test

---
+++ quasar-actors/src/main/java/co/paralleluniverse/actors/behaviors/ObjectProxyServerActor.java
@@ -0,0 +1,288 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (C) 2013, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.actors.behaviors;
+
+import co.paralleluniverse.actors.ActorBuilder;
+import co.paralleluniverse.actors.ActorRef;
+import co.paralleluniverse.actors.ActorRefDelegate;
+import co.paralleluniverse.actors.ActorUtil;
+import co.paralleluniverse.actors.LocalActor;
+import co.paralleluniverse.actors.MailboxConfig;
+import co.paralleluniverse.fibers.Joinable;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.Strand;
+import co.paralleluniverse.strands.channels.SendPort;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author pron
+ */
+public class ObjectProxyServerActor extends ServerActor<ObjectProxyServerActor.Invocation, Object, ObjectProxyServerActor.Invocation> {
+    private final Class<?>[] interfaces;
+    private final Object target;
+    private final boolean callOnVoidMethods;
+
+    public ObjectProxyServerActor(String name, Strand strand, MailboxConfig mailboxConfig, boolean callOnVoidMethods, Object target, Class<?>[] interfaces) {
+        super(name, null, 0L, null, strand, mailboxConfig);
+        this.callOnVoidMethods = callOnVoidMethods;
+        this.target = target != null ? target : this;
+        this.interfaces = interfaces != null ? Arrays.copyOf(interfaces, interfaces.length) : target.getClass().getInterfaces();
+        if (this.interfaces == null)
+            throw new IllegalArgumentException("No interfaces provided, and target of class " + target.getClass().getName() + " implements no interfaces");
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Constructors">
+    /////////// Constructors ///////////////////////////////////
+    public ObjectProxyServerActor(String name, MailboxConfig mailboxConfig, boolean callOnVoidMethods, Object target, Class<?>... interfaces) {
+        this(name, null, mailboxConfig, callOnVoidMethods, target, interfaces);
+    }
+
+    public ObjectProxyServerActor(String name, boolean callOnVoidMethods, Object target, Class<?>... interfaces) {
+        this(name, null, null, callOnVoidMethods, target, interfaces);
+    }
+
+    public ObjectProxyServerActor(MailboxConfig mailboxConfig, boolean callOnVoidMethods, Object target, Class<?>... interfaces) {
+        this(null, null, mailboxConfig, callOnVoidMethods, target, interfaces);
+    }
+
+    public ObjectProxyServerActor(boolean callOnVoidMethods, Object target, Class<?>... interfaces) {
+        this(null, null, null, callOnVoidMethods, target, interfaces);
+    }
+
+    public ObjectProxyServerActor(String name, MailboxConfig mailboxConfig, boolean callOnVoidMethods, Class<?>... interfaces) {
+        this(name, null, mailboxConfig, callOnVoidMethods, null, interfaces);
+    }
+
+    public ObjectProxyServerActor(String name, boolean callOnVoidMethods, Class<?>... interfaces) {
+        this(name, null, null, callOnVoidMethods, null, interfaces);
+    }
+
+    public ObjectProxyServerActor(MailboxConfig mailboxConfig, boolean callOnVoidMethods, Class<?>... interfaces) {
+        this(null, null, mailboxConfig, callOnVoidMethods, null, interfaces);
+    }
+
+    public ObjectProxyServerActor(boolean callOnVoidMethods, Class<?>... interfaces) {
+        this(null, null, null, callOnVoidMethods, null, interfaces);
+    }
+
+    public ObjectProxyServerActor(String name, MailboxConfig mailboxConfig, boolean callOnVoidMethods, Object target) {
+        this(name, null, mailboxConfig, callOnVoidMethods, target, null);
+    }
+
+    public ObjectProxyServerActor(String name, boolean callOnVoidMethods, Object target) {
+        this(name, null, null, callOnVoidMethods, target, null);
+    }
+
+    public ObjectProxyServerActor(MailboxConfig mailboxConfig, boolean callOnVoidMethods, Object target) {
+        this(null, null, mailboxConfig, callOnVoidMethods, target, null);
+    }
+
+    public ObjectProxyServerActor(boolean callOnVoidMethods, Object target) {
+        this(null, null, null, callOnVoidMethods, target, null);
+    }
+
+    public ObjectProxyServerActor(String name, MailboxConfig mailboxConfig, boolean callOnVoidMethods) {
+        this(name, null, mailboxConfig, callOnVoidMethods, null, null);
+    }
+
+    public ObjectProxyServerActor(String name, boolean callOnVoidMethods) {
+        this(name, null, null, callOnVoidMethods, null, null);
+    }
+
+    public ObjectProxyServerActor(MailboxConfig mailboxConfig, boolean callOnVoidMethods) {
+        this(null, null, mailboxConfig, callOnVoidMethods, null, null);
+    }
+
+    public ObjectProxyServerActor(boolean callOnVoidMethods) {
+        this(null, null, null, callOnVoidMethods, null, null);
+    }
+    //</editor-fold>
+
+    @Override
+    protected Server<Invocation, Object, Invocation> makeRef(ActorRef<Object> ref) {
+        return (Server<Invocation, Object, Invocation>) makeProxyRef(super.makeRef(ref));
+    }
+
+    private Object makeProxyRef(Server<Invocation, Object, Invocation> ref) {
+        final boolean local = ref instanceof LocalBehavior;
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(),
+                combine(interfaces, local ? standardLocalInterfaces : standardInterfaces),
+                new ObjectProxyServerImpl(local ? this : null,
+                ref,
+                callOnVoidMethods));
+    }
+    private static Class<?>[] standardInterfaces = new Class[]{
+        Server.class,
+        ActorRef.class,
+        Behavior.class,
+        SendPort.class,
+        ActorRefDelegate.class,};
+    private static Class<?>[] standardLocalInterfaces = new Class[]{
+        Server.class,
+        ActorRef.class,
+        Behavior.class,
+        SendPort.class,
+        ActorBuilder.class,
+        Joinable.class,
+        LocalBehavior.class,
+        ActorRefDelegate.class,};
+
+    private static class ObjectProxyServerImpl implements InvocationHandler, java.io.Serializable {
+        private transient final ObjectProxyServerActor actor;
+        private final boolean callOnVoidMethods;
+        private final Server<Invocation, Object, Invocation> ref;
+
+        ObjectProxyServerImpl(ObjectProxyServerActor actor, Server<Invocation, Object, Invocation> ref, boolean callOnVoidMethods) {
+            this.actor = actor;
+            this.ref = ref;
+            this.callOnVoidMethods = callOnVoidMethods;
+        }
+
+        boolean isInActor() {
+            return Objects.equals(ref, LocalActor.self());
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws SuspendExecution, Throwable {
+            final Class<?> cls = method.getDeclaringClass();
+            if (cls == ActorRefDelegate.class && method.getName().equals("getRef"))
+                return ref;
+            if (proxy instanceof LocalBehavior && method.getName().equals("writeReplace"))
+                return actor.makeProxyRef((Server<Invocation, Object, Invocation>) ((LocalBehavior) ref).writeReplace());
+            if (Arrays.asList(standardLocalInterfaces).contains(cls)) {
+                try {
+                    return method.invoke(ref, args);
+                } catch (InvocationTargetException e) {
+                    throw e.getCause();
+                }
+            }
+            if (cls == Object.class) {
+                switch (method.getName()) {
+                    case "hashCode":
+                        return Objects.hashCode(ref);
+                    case "equals":
+                        if (!(args[0] instanceof ActorRef))
+                            return false;
+                        return ActorUtil.equals(ref, (ActorRef) args[0]);
+                    case "toString":
+                        return "ObjectProxyServer{" + ref.toString() + "}";
+                    default:
+                        throw new UnsupportedOperationException();
+                }
+            }
+
+            if (isInActor()) {
+                try {
+                    return method.invoke(ServerActor.currentServerActor(), args);
+                } catch (InvocationTargetException e) {
+                    throw e.getCause();
+                }
+            } else {
+                final Invocation m = new Invocation(method, args, false);
+                if (callOnVoidMethods || (method.getReturnType() != void.class && method.getReturnType() != Void.class))
+                    return ref.call(m);
+                else {
+                    ref.cast(m);
+                    return null;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected Object handleCall(ActorRef<Object> from, Object id, Invocation m) throws Exception, SuspendExecution {
+        try {
+            Object res = m.invoke(target);
+            return res == null ? NULL_RETURN_VALUE : res;
+        } catch (InvocationTargetException e) {
+            assert !(e.getCause() instanceof SuspendExecution);
+            log().error("handleCall: Invocation " + m + " has thrown an exception.", e.getCause());
+            throw rethrow(e.getCause());
+        }
+    }
+
+    @Override
+    protected void handleCast(ActorRef<Object> from, Object id, Invocation m) throws SuspendExecution {
+        try {
+            m.invoke(target);
+        } catch (InvocationTargetException e) {
+            assert !(e.getCause() instanceof SuspendExecution);
+            log().error("handleCast: Invocation " + m + " has thrown an exception.", e.getCause());
+        }
+    }
+
+    protected static class Invocation implements java.io.Serializable {
+        private final Method method;
+        private final Object[] params;
+
+        public Invocation(Method method, List<Object> params) {
+            this.method = method;
+            this.params = params.toArray(new Object[params.size()]);
+        }
+
+        public Invocation(Method method, Object... params) {
+            this(method, params, false);
+        }
+
+        Invocation(Method method, Object[] params, boolean copy) {
+            this.method = method;
+            this.params = copy ? Arrays.copyOf(params, params.length) : params;
+        }
+
+        public Method getMethod() {
+            return method;
+        }
+
+        public List<Object> getParams() {
+            return Collections.unmodifiableList(Arrays.asList(params));
+        }
+
+        Object invoke(Object target) throws SuspendExecution, InvocationTargetException {
+            try {
+                return method.invoke(target, params);
+            } catch (IllegalAccessException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return method.toString() + Arrays.toString(params);
+        }
+    }
+
+    private static Class[] combine(Class[] ifaces1, Class[] ifaces2) {
+        final Class[] ifaces = new Class[ifaces1.length + ifaces2.length];
+        System.arraycopy(ifaces1, 0, ifaces, 0, ifaces1.length);
+        System.arraycopy(ifaces2, 0, ifaces, ifaces1.length, ifaces2.length);
+        return ifaces;
+    }
+
+    private static RuntimeException rethrow(Throwable t) throws Exception {
+        if (t instanceof Exception)
+            throw (Exception) t;
+        if (t instanceof Error)
+            throw (Error) t;
+        throw new RuntimeException(t);
+    }
+}
---
+++ quasar-actors/src/test/java/co/paralleluniverse/actors/behaviors/ObjectProxyServerTest.java
@@ -0,0 +1,348 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (C) 2013, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.paralleluniverse.actors.behaviors;
+
+import co.paralleluniverse.actors.ActorRef;
+import co.paralleluniverse.actors.BasicActor;
+import co.paralleluniverse.actors.Actor;
+import co.paralleluniverse.actors.LocalActor;
+import co.paralleluniverse.actors.MailboxConfig;
+import co.paralleluniverse.common.util.Debug;
+import co.paralleluniverse.common.util.Exceptions;
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.FiberForkJoinScheduler;
+import co.paralleluniverse.fibers.FiberScheduler;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.fibers.Suspendable;
+import co.paralleluniverse.strands.Strand;
+import co.paralleluniverse.strands.channels.Channels;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+/**
+ * These tests are also good tests for sendSync, as they test sendSync (and receive) from both fibers and threads.
+ *
+ * @author pron
+ */
+public class ObjectProxyServerTest {
+    @Rule
+    public TestName name = new TestName();
+    @Rule
+    public TestRule watchman = new TestWatcher() {
+        @Override
+        protected void starting(Description desc) {
+            if (Debug.isDebug()) {
+                System.out.println("STARTING TEST " + desc.getMethodName());
+                Debug.record(0, "STARTING TEST " + desc.getMethodName());
+            }
+        }
+
+        @Override
+        public void failed(Throwable e, Description desc) {
+            System.out.println("FAILED TEST " + desc.getMethodName() + ": " + e.getMessage());
+            e.printStackTrace(System.err);
+            if (Debug.isDebug() && !(e instanceof OutOfMemoryError)) {
+                Debug.record(0, "EXCEPTION IN THREAD " + Thread.currentThread().getName() + ": " + e + " - " + Arrays.toString(e.getStackTrace()));
+                Debug.dumpRecorder("~/quasar.dump");
+            }
+        }
+
+        @Override
+        protected void succeeded(Description desc) {
+            Debug.record(0, "DONE TEST " + desc.getMethodName());
+        }
+    };
+    static final MailboxConfig mailboxConfig = new MailboxConfig(10, Channels.OverflowPolicy.THROW);
+    private FiberScheduler scheduler;
+
+    public ObjectProxyServerTest() {
+        scheduler = new FiberForkJoinScheduler("test", 4, null, false);
+    }
+
+    private Server<?, ?, ?> spawnServer(boolean callOnVoidMethods, Object target) {
+        return new ObjectProxyServerActor("server", callOnVoidMethods, target).spawn(scheduler);
+    }
+
+    private <T extends Actor<Message, V>, Message, V> T spawnActor(T actor) {
+        Fiber fiber = new Fiber(scheduler, actor);
+        fiber.setUncaughtExceptionHandler(new Strand.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Strand s, Throwable e) {
+                e.printStackTrace();
+                throw Exceptions.rethrow(e);
+            }
+        });
+        fiber.start();
+        return actor;
+    }
+
+     @Suspendable
+    public static interface A {
+        int foo(String str, int x); // throws SuspendExecution;
+
+        void bar(int x); // throws SuspendExecution;
+    }
+
+    @Test
+    public void testShutdown() throws Exception {
+        final Server<?, ?, ?> a = spawnServer(false, new A() {
+            public int foo(String str, int x) {
+                return str.length() + x;
+            }
+
+            public void bar(int x) {
+                throw new UnsupportedOperationException();
+            }
+        });
+
+        a.shutdown();
+        LocalActor.join(a, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void whenCalledThenResultIsReturned() throws Exception {
+        final Server<?, ?, ?> a = spawnServer(false, new A() {
+            @Suspendable
+            public int foo(String str, int x) {
+                try {
+                    Strand.sleep(50);
+                    return str.length() + x;
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (SuspendExecution e) {
+                    throw new AssertionError(e);
+                }
+            }
+
+            public void bar(int x) {
+                throw new UnsupportedOperationException();
+            }
+        });
+
+        Actor<?, Integer> actor = spawnActor(new BasicActor<Object, Integer>(mailboxConfig) {
+            protected Integer doRun() throws SuspendExecution, InterruptedException {
+                return ((A) a).foo("hello", 2);
+            }
+        });
+
+        int res = actor.get();
+        assertThat(res, is(7));
+
+        a.shutdown();
+        LocalActor.join(a, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void whenCalledFromThreadThenResultIsReturned() throws Exception {
+        final Server<?, ?, ?> a = spawnServer(false, new A() {
+            @Suspendable
+            public int foo(String str, int x) {
+                try {
+                    Strand.sleep(50);
+                    return str.length() + x;
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (SuspendExecution e) {
+                    throw new AssertionError(e);
+                }
+            }
+
+            public void bar(int x) {
+                throw new UnsupportedOperationException();
+            }
+        });
+
+        int res = ((A) a).foo("hello", 2);
+        assertThat(res, is(7));
+
+        ((A) a).bar(3);
+
+        res = ((A) a).foo("hello", 2);
+        assertThat(res, is(7));
+
+        a.shutdown();
+        LocalActor.join(a, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void whenHandleCallThrowsExceptionThenItPropagatesToCaller() throws Exception {
+        final Server<?, ?, ?> a = spawnServer(false, new A() {
+            public int foo(String str, int x) {
+                throw new RuntimeException("my exception");
+            }
+
+            public void bar(int x) {
+                throw new UnsupportedOperationException();
+            }
+        });
+
+        Actor<?, Void> actor = spawnActor(new BasicActor<Object, Void>(mailboxConfig) {
+            protected Void doRun() throws SuspendExecution, InterruptedException {
+                try {
+                    int res = ((A) a).foo("hello", 2);
+                    fail();
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                    assertThat(e.getMessage(), equalTo("my exception"));
+                }
+                return null;
+            }
+        });
+
+        actor.join();
+        a.shutdown();
+        LocalActor.join(a, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void whenHandleCallThrowsExceptionThenItPropagatesToThreadCaller() throws Exception {
+        final Server<?, ?, ?> a = spawnServer(false, new A() {
+            public int foo(String str, int x) {
+                throw new RuntimeException("my exception");
+            }
+
+            public void bar(int x) {
+                throw new UnsupportedOperationException();
+            }
+        });
+
+        try {
+            int res = ((A) a).foo("hello", 2);
+            fail();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            assertThat(e.getMessage(), equalTo("my exception"));
+        }
+
+        a.shutdown();
+        LocalActor.join(a, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void testCast() throws Exception {
+        final AtomicInteger called = new AtomicInteger(0);
+        final Server<?, ?, ?> a = spawnServer(false, new A() {
+            public int foo(String str, int x) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Suspendable
+            public void bar(int x) {
+                try {
+                    Strand.sleep(100);
+                    called.set(x);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (SuspendExecution e) {
+                    throw new AssertionError(e);
+                }
+            }
+        });
+
+        ((A) a).bar(15);
+        assertThat(called.get(), is(0));
+        Thread.sleep(200);
+        assertThat(called.get(), is(15));
+
+        a.shutdown();
+        LocalActor.join(a, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void testCallOnVoidMethod() throws Exception {
+        final AtomicInteger called = new AtomicInteger(0);
+        final Server<?, ?, ?> a = spawnServer(true, new A() {
+            public int foo(String str, int x) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Suspendable
+            public void bar(int x) {
+                try {
+                    Strand.sleep(100);
+                    called.set(x);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (SuspendExecution e) {
+                    throw new AssertionError(e);
+                }
+            }
+        });
+
+        ((A) a).bar(15);
+        assertThat(called.get(), is(15));
+
+        a.shutdown();
+        LocalActor.join(a, 100, TimeUnit.MILLISECONDS);
+    }
+
+    @Test
+    public void whenCalledAndTimeoutThenThrowTimeout() throws Exception {
+        final Server<?, ?, ?> a = spawnServer(false, new A() {
+            @Suspendable
+            public int foo(String str, int x) {
+                try {
+                    Strand.sleep(100);
+                    return str.length() + x;
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (SuspendExecution e) {
+                    throw new AssertionError(e);
+                }
+            }
+
+            public void bar(int x) {
+                throw new UnsupportedOperationException();
+            }
+        });
+
+        a.setDefaultTimeout(50, TimeUnit.MILLISECONDS);
+
+        try {
+            int res = ((A) a).foo("hello", 2);
+            fail("res: " + res);
+        } catch (RuntimeException e) {
+            assertThat(e.getCause(), instanceOf(TimeoutException.class));
+        }
+
+        a.setDefaultTimeout(200, TimeUnit.MILLISECONDS);
+
+        int res = ((A) a).foo("hello", 2);
+        assertThat(res, is(7));
+
+        a.shutdown();
+        LocalActor.join(a, 500, TimeUnit.MILLISECONDS);
+    }
+}
