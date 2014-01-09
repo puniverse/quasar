c9c602ca636102c1f9fdb5d5655d2556c9e9a86e
pron <ron.pressler@gmail.com>
2014-01-09 13:03:48 +0200

    Copyright notice [ci skip]

--- quasar-core/src/main/java/co/paralleluniverse/io/serialization/kryo/KryoSerializer.java
+++ quasar-core/src/main/java/co/paralleluniverse/io/serialization/kryo/KryoSerializer.java
@@ -1,5 +1,4 @@
 /*
- * Quasar: lightweight threads and actors for the JVM.
  * Copyright (c) 2013-2014, Parallel Universe Software Co. All rights reserved.
  * 
  * This program and the accompanying materials are dual-licensed under
