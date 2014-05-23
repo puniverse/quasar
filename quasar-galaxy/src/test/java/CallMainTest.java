fbb96574f10c5038c851323a807b3350cb37e3cf
pron <ron.pressler@gmail.com>
2014-05-23 15:48:18 +0300

    -

--- quasar-galaxy/src/test/java/CallMainTest.java
+++ quasar-galaxy/src/test/java/CallMainTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import co.paralleluniverse.galaxy.example.testing.Peer1;
 import org.junit.After;
 import org.junit.AfterClass;
