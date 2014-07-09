eb3e40c4ce7027eb7f3e22b09c8ce96311e84b40
eitan <eitan@paralleluniverse.co>
2014-07-09 18:42:28 +0300

    test simpleScanner

---
+++ quasar-core/src/test/java/co/paralleluniverse/fibers/instrument/ScannerTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.paralleluniverse.fibers.instrument;
+
+import co.paralleluniverse.common.reflection.ClassLoaderUtil;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author eitan
+ */
+public class ScannerTest {
+
+    @Test
+    public void suspendablesMethodWithDescsTest() {
+        String path = ClassLoader.getSystemClassLoader().getResource("suspendables.test").getPath();
+        SimpleSuspendableClassifier ssc = new SimpleSuspendableClassifier(path);
+        final String name = ClassLoaderUtil.classToSlashed("co.paralleluniverse.fibers.instrument.ScannerTest");
+        assertEquals(true, ssc.isSuspendable(name, "foo", "V"));
+        assertEquals(true, ssc.isSuspendable(name, "bar", "(I)V"));
+        assertEquals(false, ssc.isSuspendable(name, "bar", "(D)V"));
+    }
+
+    @Test
+    public void suspendablesClassesTest() {
+        String path = ClassLoader.getSystemClassLoader().getResource("suspendables.test").getPath();
+        SimpleSuspendableClassifier ssc = new SimpleSuspendableClassifier(path);
+        final String name = ClassLoaderUtil.classToSlashed("co.paralleluniverse.fibers.instrument.ScannerTestB");
+        assertEquals(true, ssc.isSuspendable(name, "anyMethod", "V"));
+    }
+}
