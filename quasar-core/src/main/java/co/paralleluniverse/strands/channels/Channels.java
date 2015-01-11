fb8788f939fd5edec30a1c9c17f5ce0050267647
circlespainter <fabio@paralleluniverse.co>
2015-01-11 20:29:03 +0100

    (C) updates [ci skip]

--- quasar-core/src/main/java/co/paralleluniverse/strands/channels/Channels.java
+++ quasar-core/src/main/java/co/paralleluniverse/strands/channels/Channels.java
@@ -1,6 +1,6 @@
 /*
  * Quasar: lightweight strands and actors for the JVM.
- * Copyright (c) 2013-2014, Parallel Universe Software Co. All rights reserved.
+ * Copyright (c) 2013-2015, Parallel Universe Software Co. All rights reserved.
  * 
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
