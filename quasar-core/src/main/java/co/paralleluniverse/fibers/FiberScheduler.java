64e5338c1b1abdc4a808d86bff4c0f3fc96fb6e0
circlespainter <fabio@paralleluniverse.co>
2015-01-19 23:46:40 +0100

    :fire: 'FiberScheduler' now implements 'StrandFactory', remvove 'DefaultFiberFactory'

--- quasar-core/src/main/java/co/paralleluniverse/fibers/FiberScheduler.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/FiberScheduler.java
@@ -14,6 +14,8 @@
 package co.paralleluniverse.fibers;
 
 import co.paralleluniverse.common.monitoring.MonitorType;
+import co.paralleluniverse.strands.Strand;
+import co.paralleluniverse.strands.StrandFactory;
 import co.paralleluniverse.strands.SuspendableCallable;
 import java.util.Map;
 import java.util.concurrent.Executor;
@@ -25,7 +27,7 @@ import java.util.concurrent.TimeUnit;
  *
  * @author pron
  */
-public abstract class FiberScheduler implements FiberFactory {
+public abstract class FiberScheduler implements FiberFactory, StrandFactory {
     static final FibersMonitor NOOP_FIBERS_MONITOR = new NoopFibersMonitor();
     private final String name;
     private final FibersMonitor fibersMonitor;
@@ -63,6 +65,11 @@ public abstract class FiberScheduler implements FiberFactory {
         return new Fiber<T>(this, target);
     }
 
+    @Override
+    public Strand newStrand(final SuspendableCallable<?> target) {
+        return newFiber(target);
+    }
+    
     abstract Future<Void> schedule(Fiber<?> fiber, Object blocker, long delay, TimeUnit unit);
 
     abstract Map<Thread, Fiber> getRunningFibers();
