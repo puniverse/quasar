c72e9b50134a7be79891596ae9f2a53047c0f0d4
pron <ron.pressler@gmail.com>
2015-05-27 13:22:09 +0300

    -

--- quasar-core/src/main/java/co/paralleluniverse/fibers/io/FiberSelectSocketChannel.java
+++ quasar-core/src/main/java/co/paralleluniverse/fibers/io/FiberSelectSocketChannel.java
@@ -34,192 +34,192 @@ import java.util.Set;
  * @author circlespainter
  */
 public class FiberSelectSocketChannel implements ByteChannel, ScatteringByteChannel, GatheringByteChannel, NetworkChannel {
-	private final SocketChannel sc;
-	private SelectionKey key;
+    private final SocketChannel sc;
+    private SelectionKey key;
 
-        public long openTime, registrationTime, connectTime, writeTime, readTime, deregistrationTime, closeTime;
+    public long openTime, registrationTime, connectTime, writeTime, readTime, deregistrationTime, closeTime;
 
-	FiberSelectSocketChannel(final SocketChannel sc) throws IOException {
-		this.sc = sc;
-		this.sc.configureBlocking(false);
-	}
+    FiberSelectSocketChannel(final SocketChannel sc) throws IOException {
+        this.sc = sc;
+        this.sc.configureBlocking(false);
+    }
 
-	@Suspendable
-	public static FiberSelectSocketChannel open() throws IOException, SuspendExecution {
-		// TODO can block the thread, make more efficient
-            long start = System.nanoTime();
-		FiberSelectSocketChannel c = new FiberSelectSocketChannel(SocketChannel.open());
-                long end = System.nanoTime();
-                c.openTime = end - start;
+    @Suspendable
+    public static FiberSelectSocketChannel open() throws IOException, SuspendExecution {
+        // TODO can block the thread, make more efficient
+        long start = System.nanoTime();
+        FiberSelectSocketChannel c = new FiberSelectSocketChannel(SocketChannel.open());
+        long end = System.nanoTime();
+        c.openTime = end - start;
 
-                start = System.nanoTime();
-		c.register();
-                end = System.nanoTime();
-                c.registrationTime = end - start;
-		return c;
-	}
+        start = System.nanoTime();
+        c.register();
+        end = System.nanoTime();
+        c.registrationTime = end - start;
+        return c;
+    }
 
-	public static FiberSelectSocketChannel open(final SocketAddress sa) throws IOException, SuspendExecution {
-		final FiberSelectSocketChannel res = open();
-		res.connect(sa);
-		return res;
-	}
+    public static FiberSelectSocketChannel open(final SocketAddress sa) throws IOException, SuspendExecution {
+        final FiberSelectSocketChannel res = open();
+        res.connect(sa);
+        return res;
+    }
 
-	private void register() throws IOException, SuspendExecution {
-		this.key = FiberSelect.register(sc);
-	}
+    private void register() throws IOException, SuspendExecution {
+        this.key = FiberSelect.register(sc);
+    }
 
-	public void connect(SocketAddress sa) throws IOException, SuspendExecution {
-            long start = System.nanoTime();
-		sc.connect(sa);
-		while (!sc.finishConnect())
-			Fiber.park(key);
-                long end = System.nanoTime();
-                connectTime = end - start;
-	}
+    public void connect(SocketAddress sa) throws IOException, SuspendExecution {
+        long start = System.nanoTime();
+        sc.connect(sa);
+        while (!sc.finishConnect())
+            Fiber.park(key);
+        long end = System.nanoTime();
+        connectTime = end - start;
+    }
 
-	@Override
-	public boolean isOpen() {
-		return sc.isOpen();
-	}
+    @Override
+    public boolean isOpen() {
+        return sc.isOpen();
+    }
 
-	@Override
-	public void close() throws IOException {
-            long start = System.nanoTime();
-		// TOOO if it can block run on pool
-		FiberSelect.deregister(key);
-                long end = System.nanoTime();
-                deregistrationTime = end - start;
-                start = System.nanoTime();
-		sc.close();
-                end = System.nanoTime();
-                closeTime = end - start;
-	}
+    @Override
+    public void close() throws IOException {
+        long start = System.nanoTime();
+        // TOOO if it can block run on pool
+        FiberSelect.deregister(key);
+        long end = System.nanoTime();
+        deregistrationTime = end - start;
+        start = System.nanoTime();
+        sc.close();
+        end = System.nanoTime();
+        closeTime = end - start;
+    }
 
-	@Override
-	@Suspendable
-	public int read(final ByteBuffer dst) throws IOException {
-            long start = System.nanoTime();
-		try {
-			int count;
-			while ((count = sc.read(dst)) == 0)
-				Fiber.park(key);
-                        long end = System.nanoTime();
-                        readTime += end - start;
-			return count;
-		} catch (SuspendExecution e) {
-			throw new AssertionError(e);
-		}
-	}
+    @Override
+    @Suspendable
+    public int read(final ByteBuffer dst) throws IOException {
+        long start = System.nanoTime();
+        try {
+            int count;
+            while ((count = sc.read(dst)) == 0)
+                Fiber.park(key);
+            long end = System.nanoTime();
+            readTime += end - start;
+            return count;
+        } catch (SuspendExecution e) {
+            throw new AssertionError(e);
+        }
+    }
 
-	@Override
-	@Suspendable
-	public long read(final ByteBuffer[] dsts) throws IOException {
-            long start = System.nanoTime();
-		try {
-			long count;
-			while ((count = sc.read(dsts)) == 0)
-				Fiber.park(key);
-                        long end = System.nanoTime();
-                        readTime += end - start;
-			return count;
-		} catch (SuspendExecution e) {
-			throw new AssertionError(e);
-		}
-	}
+    @Override
+    @Suspendable
+    public long read(final ByteBuffer[] dsts) throws IOException {
+        long start = System.nanoTime();
+        try {
+            long count;
+            while ((count = sc.read(dsts)) == 0)
+                Fiber.park(key);
+            long end = System.nanoTime();
+            readTime += end - start;
+            return count;
+        } catch (SuspendExecution e) {
+            throw new AssertionError(e);
+        }
+    }
 
-	@Override
-	@Suspendable
-	public long read(final ByteBuffer[] dsts, final int offset, final int length) throws IOException {
-            long start = System.nanoTime();
-		try {
-			long count;
-			while ((count = sc.read(dsts, offset, length)) == 0)
-				Fiber.park(key);
-                        long end = System.nanoTime();
-                        readTime += end - start;
-			return count;
-		} catch (SuspendExecution e) {
-			throw new AssertionError(e);
-		}
-	}
+    @Override
+    @Suspendable
+    public long read(final ByteBuffer[] dsts, final int offset, final int length) throws IOException {
+        long start = System.nanoTime();
+        try {
+            long count;
+            while ((count = sc.read(dsts, offset, length)) == 0)
+                Fiber.park(key);
+            long end = System.nanoTime();
+            readTime += end - start;
+            return count;
+        } catch (SuspendExecution e) {
+            throw new AssertionError(e);
+        }
+    }
 
-	@Override
-	@Suspendable
-	public int write(final ByteBuffer src) throws IOException {
-            long start = System.nanoTime();
-		try {
-			int count;
-			while ((count = sc.write(src)) == 0)
-				Fiber.park(key);
-                        long end = System.nanoTime();
-                        writeTime += end - start;
-			return count;
-		} catch (SuspendExecution e) {
-			throw new AssertionError(e);
-		}
-	}
+    @Override
+    @Suspendable
+    public int write(final ByteBuffer src) throws IOException {
+        long start = System.nanoTime();
+        try {
+            int count;
+            while ((count = sc.write(src)) == 0)
+                Fiber.park(key);
+            long end = System.nanoTime();
+            writeTime += end - start;
+            return count;
+        } catch (SuspendExecution e) {
+            throw new AssertionError(e);
+        }
+    }
 
-	@Override
-	@Suspendable
-	public long write(final ByteBuffer[] srcs) throws IOException {
-            long start = System.nanoTime();
-		try {
-			long count;
-			while ((count = sc.write(srcs)) == 0)
-				Fiber.park(key);
-                        long end = System.nanoTime();
-                        writeTime += end - start;
-			return count;
-		} catch (SuspendExecution e) {
-			throw new AssertionError(e);
-		}
-	}
+    @Override
+    @Suspendable
+    public long write(final ByteBuffer[] srcs) throws IOException {
+        long start = System.nanoTime();
+        try {
+            long count;
+            while ((count = sc.write(srcs)) == 0)
+                Fiber.park(key);
+            long end = System.nanoTime();
+            writeTime += end - start;
+            return count;
+        } catch (SuspendExecution e) {
+            throw new AssertionError(e);
+        }
+    }
 
-	@Override
-	@Suspendable
-	public long write(final ByteBuffer[] srcs, int offset, int length) throws IOException {
-            long start = System.nanoTime();
-		try {
-			long count;
-			while ((count = sc.write(srcs, offset, length)) == 0)
-				Fiber.park(key);
-                        long end = System.nanoTime();
-                        writeTime += end - start;
-			return count;
-		} catch (SuspendExecution e) {
-			throw new AssertionError(e);
-		}
-	}
+    @Override
+    @Suspendable
+    public long write(final ByteBuffer[] srcs, int offset, int length) throws IOException {
+        long start = System.nanoTime();
+        try {
+            long count;
+            while ((count = sc.write(srcs, offset, length)) == 0)
+                Fiber.park(key);
+            long end = System.nanoTime();
+            writeTime += end - start;
+            return count;
+        } catch (SuspendExecution e) {
+            throw new AssertionError(e);
+        }
+    }
 
-	@Override
-	public NetworkChannel bind(final SocketAddress local) throws IOException {
-		// TOOO if it can block run on pool
-		sc.bind(local);
-		return this;
-	}
+    @Override
+    public NetworkChannel bind(final SocketAddress local) throws IOException {
+        // TOOO if it can block run on pool
+        sc.bind(local);
+        return this;
+    }
 
-	@Override
-	public SocketAddress getLocalAddress() throws IOException {
-		// TOOO if it can block run on pool
-		return sc.getLocalAddress();
-	}
+    @Override
+    public SocketAddress getLocalAddress() throws IOException {
+        // TOOO if it can block run on pool
+        return sc.getLocalAddress();
+    }
 
-	@Override
-	public <T> NetworkChannel setOption(final SocketOption<T> name, final T value) throws IOException {
-		// TOOO if it can block run on pool
-		sc.setOption(name, value);
-		return this;
-	}
+    @Override
+    public <T> NetworkChannel setOption(final SocketOption<T> name, final T value) throws IOException {
+        // TOOO if it can block run on pool
+        sc.setOption(name, value);
+        return this;
+    }
 
-	@Override
-	public <T> T getOption(final SocketOption<T> name) throws IOException {
-		// TOOO if it can block run on pool
-		return sc.getOption(name);
-	}
+    @Override
+    public <T> T getOption(final SocketOption<T> name) throws IOException {
+        // TOOO if it can block run on pool
+        return sc.getOption(name);
+    }
 
-	@Override
-	public Set<SocketOption<?>> supportedOptions() {
-		return sc.supportedOptions();
-	}
+    @Override
+    public Set<SocketOption<?>> supportedOptions() {
+        return sc.supportedOptions();
+    }
 }
