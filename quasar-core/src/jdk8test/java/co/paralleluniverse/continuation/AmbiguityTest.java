eb8dcb54f948ea45f160989e242ff053b37d28fb
pron <ron.pressler@gmail.com>
2015-07-13 22:30:31 +0300

    add amb and tests

---
+++ quasar-core/src/jdk8test/java/co/paralleluniverse/continuation/AmbiguityTest.java
@@ -0,0 +1,121 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2013-2015, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.continuation;
+
+import static co.paralleluniverse.continuation.Ambiguity.*;
+import static co.paralleluniverse.continuation.CoIterable.*;
+import com.google.common.collect.Iterables;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pron
+ */
+public class AmbiguityTest {
+
+    public AmbiguityTest() {
+    }
+
+    @Test
+    public void test1() throws Exception {
+        Ambiguity<Integer> amb = solve(() -> {
+            int a = amb(1, 2, 3);
+            return a;
+        });
+
+        assertEquals((Integer) 1, amb.run());
+        assertTrue(amb.hasRemaining());
+        assertEquals((Integer) 2, amb.run());
+        assertTrue(amb.hasRemaining());
+        assertEquals((Integer) 3, amb.run());
+        assertTrue(!amb.hasRemaining());
+        try {
+            amb.run();
+            fail();
+        } catch (NoSolution e) {
+        }
+    }
+
+    @Test
+    public void test2() {
+        Ambiguity<Integer> amb = solve(() -> {
+            int a = amb(1, 2, 3);
+            return a;
+        });
+
+        assertEquals(list(1, 2, 3), list(solutions(amb)));
+    }
+
+    @Test
+    public void test3() {
+        Ambiguity<String> amb = solve(() -> {
+            int a = amb(1, 2);
+            String b = amb("a", "b");
+            return b + a;
+        });
+
+        assertEquals(list("a1", "b1", "a2", "b2"), list(solutions(amb)));
+    }
+
+    @Test
+    public void test4() {
+        Ambiguity<Integer> amb = solve(() -> {
+            int a = amb(1, 2, 3);
+            int b = amb(2, 3, 4);
+
+            assertThat(b < a);
+            return b;
+        });
+
+        assertEquals(list(2), list(solutions(amb)));
+    }
+
+    @Test
+    public void test5() {
+        Ambiguity<Integer> amb = solve(() -> {
+            int a = amb(1, 2, 3);
+            int b = amb(3, 4);
+
+            assertThat(b < a);
+            return b;
+        });
+
+        assertEquals(list(), list(solutions(amb)));
+    }
+
+    static <T> Iterable<T> solutions(Ambiguity<T> amb) {
+        return iterable(() -> {
+            try {
+                while (amb.hasRemaining())
+                    produce(amb.run());
+            } catch (NoSolution e) {
+            }
+        });
+    }
+
+    private static <E> List<E> list(Iterable<E> it) {
+        List<E> list = new ArrayList<E>();
+        Iterables.addAll(list, it);
+        return list;
+    }
+
+    private static <E> List<E> list(E... xs) {
+        return new ArrayList<E>(Arrays.asList(xs));
+    }
+}
---
+++ quasar-core/src/main/java/co/paralleluniverse/continuation/Ambiguity.java
@@ -0,0 +1,131 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2013-2015, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.continuation;
+
+import co.paralleluniverse.fibers.Callable;
+import co.paralleluniverse.fibers.CalledCC;
+import co.paralleluniverse.fibers.Continuation;
+import co.paralleluniverse.fibers.Suspend;
+import static co.paralleluniverse.fibers.Continuation.suspend;
+import co.paralleluniverse.fibers.Suspendable;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ *
+ * @author pron
+ */
+public class Ambiguity<T> {
+    public static <T> Ambiguity<T> solve(Ambiguous<T> f) {
+        return new Ambiguity<T>(f);
+    }
+
+    private final Deque<AmbContinuation<T>> cs = new ArrayDeque<>();
+
+    public Ambiguity(Ambiguous<T> ambiguity) {
+        push(new AmbContinuation<T>(this, ambiguity));
+    }
+
+    public T run() throws NoSolution {
+        try {
+            Continuation<AmbScope, T> c = pop().run();
+            assert c.isDone();
+            return c.getResult();
+        } catch (RuntimeNoSolution e) {
+            throw new NoSolution();
+        }
+    }
+
+    public boolean hasRemaining() {
+        return !cs.isEmpty();
+    }
+
+    private void push(AmbContinuation<?> c) {
+        cs.addFirst((AmbContinuation<T>) c);
+    }
+
+    private AmbContinuation<T> pop() {
+        if (cs.isEmpty())
+            throw new RuntimeNoSolution();
+        return cs.removeFirst();
+    }
+
+    private static class AmbContinuation<T> extends Continuation<AmbScope, T> {
+        private final Ambiguity<T> ambiguity;
+
+        public AmbContinuation(Ambiguity<T> ambiguity, final Ambiguous<T> target) {
+            super(AmbScope.class, new Callable<T>() {
+                @Suspendable
+                @Override
+                public T call() {
+                    return target.run();
+                }
+            });
+            this.ambiguity = ambiguity;
+        }
+    }
+
+    public static <T> T amb(List<T> values) throws AmbScope {
+        AmbContinuation<?> c = captureCurrentContinuation();
+        final T v = values.remove(0);
+        if (!values.isEmpty())
+            c.ambiguity.push(c);
+        return v;
+    }
+
+    public static <T> T amb(T... values) throws AmbScope {
+        return amb(new ArrayList<>(Arrays.asList(values)));
+    }
+
+    public static void assertThat(boolean pred) throws AmbScope {
+        if (!pred)
+            suspend(SCOPE, BACKTRACK);
+    }
+
+    private static AmbContinuation<?> captureCurrentContinuation() throws AmbScope {
+        return (AmbContinuation<?>) suspend(SCOPE, CONTINUE).clone();
+    }
+
+    public static class AmbScope extends Suspend {
+    }
+
+    private static final AmbScope SCOPE = new AmbScope();
+
+    private static final CalledCC<AmbScope> CONTINUE = new CalledCC<AmbScope>() {
+        @Override
+        public <T> Continuation<AmbScope, T> suspended(Continuation<AmbScope, T> c) {
+            return c;
+        }
+    };
+
+    private static final CalledCC<AmbScope> BACKTRACK = new CalledCC<AmbScope>() {
+        @Override
+        public <T> Continuation<AmbScope, T> suspended(Continuation<AmbScope, T> c) {
+            return ((AmbContinuation<T>) c).ambiguity.pop();
+        }
+    };
+
+    public static class NoSolution extends Exception {
+    }
+
+    private static class RuntimeNoSolution extends RuntimeException {
+    }
+
+    public static interface Ambiguous<T> {
+        T run() throws AmbScope; // Unfortunately, throwables can't be generic. We would have wanted to CoIteratorScope<E>
+    }
+}
