20cf9b76c697b9a770442f3bdf97d2ee17eb5ee3
circlespainter <fabio@paralleluniverse.co>
2015-08-14 15:47:33 +0300

    Remove inlining as workaround for #112 and keep only the first API design

--- quasar-kotlin/src/main/kotlin/co/paralleluniverse/kotlin/Kotlin.kt
+++ quasar-kotlin/src/main/kotlin/co/paralleluniverse/kotlin/Kotlin.kt
@@ -24,7 +24,7 @@ import java.util.concurrent.TimeUnit
 /**
  * @author circlespainter
  */
-Suspendable public inline fun fiber<T>(start: Boolean, name: String?, scheduler: FiberScheduler?, stackSize: Int, inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> {
+Suspendable public fun fiber<T>(start: Boolean, name: String?, scheduler: FiberScheduler?, stackSize: Int, block: () -> T): Fiber<T> {
     val sc = @Suspendable object : SuspendableCallable<T> {
         @throws(SuspendExecution::class) Suspendable override fun run(): T = block()
     }
@@ -36,19 +36,19 @@ Suspendable public inline fun fiber<T>(start: Boolean, name: String?, scheduler:
     if (start) ret.start()
     return ret
 }
-Suspendable public inline fun  fiber<T>(inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> =
+Suspendable public fun  fiber<T>(block: () -> T): Fiber<T> =
     fiber(true, null, null, -1, block)
-Suspendable public inline fun  fiber<T>(start: Boolean, inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> =
+Suspendable public fun  fiber<T>(start: Boolean, block: () -> T): Fiber<T> =
     fiber(start, null, null, -1, block)
-Suspendable public inline fun  fiber<T>(name: String, inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> =
+Suspendable public fun  fiber<T>(name: String, block: () -> T): Fiber<T> =
     fiber(true, name, null, -1, block)
-Suspendable public inline fun  fiber<T>(scheduler: FiberScheduler, inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> =
+Suspendable public fun  fiber<T>(scheduler: FiberScheduler, block: () -> T): Fiber<T> =
     fiber(true, null, scheduler, -1, block)
-Suspendable public inline fun  fiber<T>(name: String, scheduler: FiberScheduler, inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> =
+Suspendable public fun  fiber<T>(name: String, scheduler: FiberScheduler, block: () -> T): Fiber<T> =
     fiber(true, name, scheduler, -1, block)
-Suspendable public inline fun  fiber<T>(start: Boolean, scheduler: FiberScheduler, inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> =
+Suspendable public fun  fiber<T>(start: Boolean, scheduler: FiberScheduler, block: () -> T): Fiber<T> =
     fiber(start, null, scheduler, -1, block)
-Suspendable public inline fun  fiber<T>(start: Boolean, name: String, scheduler: FiberScheduler, inlineOptions(InlineOption.ONLY_LOCAL_RETURN) block: () -> T): Fiber<T> =
+Suspendable public fun  fiber<T>(start: Boolean, name: String, scheduler: FiberScheduler, block: () -> T): Fiber<T> =
     fiber(start, name, scheduler, -1, block)
 
 public abstract data class SelectOp<M>(private val wrappedSA: SelectAction<M>) {
@@ -63,7 +63,7 @@ public data class Receive<M>(public val receivePort: ReceivePort<M>) : SelectOp<
 }
 public data class Send<M>(public val sendPort: SendPort<M>, public val msg: M) : SelectOp<M>(Selector.send(sendPort, msg))
 
-Suspendable public inline fun select<R, M>(actions: List<SelectOp<M>>, b: (SelectOp<M>?) -> R, priority: Boolean = false, timeout: Int = -1, unit: TimeUnit = TimeUnit.MILLISECONDS): R {
+Suspendable public fun select<R, M>(actions: List<SelectOp<M>>, b: (SelectOp<M>?) -> R, priority: Boolean = false, timeout: Int = -1, unit: TimeUnit = TimeUnit.MILLISECONDS): R {
     val sa = Selector.select(priority, timeout.toLong(), unit, actions.map{it.getWrappedSelectAction()}.toList())
     if (sa != null) {
         val sOp = actions.get(sa.index())
@@ -74,50 +74,8 @@ Suspendable public inline fun select<R, M>(actions: List<SelectOp<M>>, b: (Selec
     } else
         return b(null)
 }
-Suspendable public inline fun select<R, M>(vararg actions: SelectOp<M>, b: (SelectOp<M>?) -> R): R = select(actions.toList(), b)
-Suspendable public inline fun select<R, M>(timeout: Int, unit: TimeUnit, vararg actions: SelectOp<M>, b: (SelectOp<M>?) -> R): R =
+Suspendable public fun select<R, M>(vararg actions: SelectOp<M>, b: (SelectOp<M>?) -> R): R = select(actions.toList(), b)
+Suspendable public fun select<R, M>(timeout: Int, unit: TimeUnit, vararg actions: SelectOp<M>, b: (SelectOp<M>?) -> R): R =
     select(actions.toList(), b, false, timeout, unit)
-Suspendable public inline fun select<R, M>(priority: Boolean, timeout: Int, unit: TimeUnit, vararg actions: SelectOp<M>, b: (SelectOp<M>?) -> R): R =
+Suspendable public fun select<R, M>(priority: Boolean, timeout: Int, unit: TimeUnit, vararg actions: SelectOp<M>, b: (SelectOp<M>?) -> R): R =
     select(actions.toList(), b, priority, timeout, unit)
-
-public data class On2<R, M, O : SelectOp<M>>(public val op: O, public val handler: (O) -> R)
-@suppress("UNCHECKED_CAST")
-Suspendable public inline fun select2<R, M>(actions: List<On2<R, M, *>>, elseHandler: () -> R, priority: Boolean = false, timeout: Int = -1, unit: TimeUnit = TimeUnit.MILLISECONDS): R {
-    val sa = Selector.select(priority, timeout.toLong(), unit, actions.map{it.op.getWrappedSelectAction() as SelectAction<M>}.toList())
-    if (sa != null) {
-        val sOp = actions.get(sa.index())
-        when (sOp.op) {
-            is Receive -> (sOp.op as Receive<M>).msg = sa.message()
-        }
-        return (sOp.handler as (SelectOp<M>) -> R)(sOp.op as (SelectOp<M>))
-    } else
-        return elseHandler()
-}
-@suppress("NOTHING_TO_INLINE")
-@suppress("UNCHECKED_CAST")
-Suspendable public inline fun select2<R, M>(vararg actions: On2<R, M, *>): R =
-    select2(actions.toList(), /* Fake, never used */ { (actions.get(0).handler as (SelectOp<M>) -> R)(actions.get(0).op as SelectOp<M>) })
-Suspendable public inline fun select2<R, M>(timeout: Int, unit: TimeUnit, elseHandler: () -> R, vararg actions: On2<R, M, *>): R =
-    select2(actions.toList(), elseHandler, false, timeout, unit)
-Suspendable public inline fun select2<R, M>(priority: Boolean, timeout: Int, unit: TimeUnit, elseHandler: () -> R, vararg actions: On2<R, M, *>): R =
-    select2(actions.toList(), elseHandler, priority, timeout, unit)
-
-public data class On3<R, M>(public val op: SelectOp<M>, public val handler: (SelectOp<M>) -> R)
-Suspendable public inline fun select3<R, M>(actions: List<On3<R, M>>, elseHandler: () -> R, priority: Boolean = false, timeout: Int = -1, unit: TimeUnit = TimeUnit.MILLISECONDS): R {
-    val sa = Selector.select(priority, timeout.toLong(), unit, actions.map{it.op.getWrappedSelectAction() as SelectAction<M>}.toList())
-    if (sa != null) {
-        val sOp = actions.get(sa.index())
-        when (sOp.op) {
-            is Receive -> sOp.op.msg = sa.message()
-        }
-        return sOp.handler(sOp.op)
-    } else
-        return elseHandler()
-}
-@suppress("NOTHING_TO_INLINE")
-Suspendable public inline fun select3<R, M>(vararg actions: On3<R, M>): R =
-        select3(actions.toList(), /* Fake, never used */ { actions.get(0).handler(actions.get(0).op) })
-Suspendable public inline fun select3<R, M>(timeout: Int, unit: TimeUnit, elseHandler: () -> R, vararg actions: On3<R, M>): R =
-        select3(actions.toList(), elseHandler, false, timeout, unit)
-Suspendable public inline fun select3<R, M>(priority: Boolean, timeout: Int, unit: TimeUnit, elseHandler: () -> R, vararg actions: On3<R, M>): R =
-        select3(actions.toList(), elseHandler, priority, timeout, unit)
--- quasar-kotlin/src/test/kotlin/co/paralleluniverse/kotlin/fibers/FiberKotlinTest.kt
+++ quasar-kotlin/src/test/kotlin/co/paralleluniverse/kotlin/fibers/FiberKotlinTest.kt
@@ -32,7 +32,7 @@ import java.util.concurrent.TimeUnit
  * @author circlespainter
  */
 public class FiberKotlinTest {
-    Test public fun testSelect1() {
+    Test public fun testSelect() {
         val ch1 = Channels.newChannel<Int>(1)
         val ch2 = Channels.newChannel<Int>(1)
 
@@ -66,76 +66,6 @@ public class FiberKotlinTest {
                         else -> -1
                     }
                 }
-            }.get() == -1
-        )
-    }
-
-    Test public fun testSelect2() {
-        val ch1 = Channels.newChannel<Int>(1)
-        val ch2 = Channels.newChannel<Int>(1)
-        assertTrue (
-            fiber {
-                select2 (
-                    On2<SelectOp<Int>, Int, Receive<Int>>(Receive(ch1), { it }),
-                    On2<SelectOp<Int>, Int, Send<Int>>(Send(ch2, 2), { it })
-                )
-            }.get() is Send<*>
-        )
-
-        ch1.send(1)
-
-        assertTrue (
-            fiber {
-                select2<Int, Int> (
-                    On2(Receive(ch1), { it.msg }),
-                    On2(Send(ch2, 2), { 0 })
-                )
-            }.get() == 1
-        )
-
-        assertTrue (
-            fiber {
-                select2<Int, Int> (
-                    10, TimeUnit.MILLISECONDS,
-                    { - 1},
-                    On2(Receive(ch1), { it.msg }),
-                    On2(Send(ch2, 2), { 0 })
-                )
-            }.get() == -1
-        )
-    }
-
-    Test public fun testSelect3() {
-        val ch1 = Channels.newChannel<Int>(1)
-        val ch2 = Channels.newChannel<Int>(1)
-        assertTrue (
-            fiber {
-                select3 (
-                    On3<SelectOp<Int>, Int>(Receive(ch1), { (it as Receive<Int>) }),
-                    On3(Send(ch2, 2), { it })
-                )
-            }.get() is Send<*>
-        )
-
-        ch1.send(1)
-
-        assertTrue (
-            fiber {
-                select3 (
-                    On3(Receive(ch1), { (it as Receive<Int>).msg }),
-                    On3(Send(ch2, 2), { 0 })
-                )
-            }.get() == 1
-        )
-
-        assertTrue (
-            fiber {
-                select3 (
-                    10, TimeUnit.MILLISECONDS,
-                    { - 1},
-                    On3(Receive(ch1), { (it as Receive<Int>).msg }),
-                    On3(Send(ch2, 2), { 0 })
-                )
             }.get() == -1
         )
     }
