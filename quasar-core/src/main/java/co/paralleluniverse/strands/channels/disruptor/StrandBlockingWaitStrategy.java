d53f1eecdc0d2d6f47b0a18cd7e8789f1845b074
SkyWitit <sercan_karaoglu@yahoo.com>
2015-11-04 00:39:55 +0300

    DisruptorReceiveChannel fix

--- quasar-core/src/main/java/co/paralleluniverse/strands/channels/disruptor/StrandBlockingWaitStrategy.java
+++ quasar-core/src/main/java/co/paralleluniverse/strands/channels/disruptor/StrandBlockingWaitStrategy.java
@@ -1,41 +1,37 @@
 /*
- * Quasar: lightweight threads and actors for the JVM.
- * Copyright (c) 2013-2014, Parallel Universe Software Co. All rights reserved.
+ * Quasar: lightweight threads and actors for the JVM. Copyright (c) 2013-2014, Parallel Universe Software Co. All rights reserved.
  * 
- * This program and the accompanying materials are dual-licensed under
- * either the terms of the Eclipse Public License v1.0 as published by
- * the Eclipse Foundation
- *  
- *   or (per the licensee's choosing)
- *  
- * under the terms of the GNU Lesser General Public License version 3.0
- * as published by the Free Software Foundation.
+ * This program and the accompanying materials are dual-licensed under either the terms of the Eclipse Public License v1.0 as published by the Eclipse
+ * Foundation
+ * 
+ * or (per the licensee's choosing)
+ * 
+ * under the terms of the GNU Lesser General Public License version 3.0 as published by the Free Software Foundation.
  */
 /*
  * Copyright 2011 LMAX Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
  */
 package co.paralleluniverse.strands.channels.disruptor;
 
-import co.paralleluniverse.fibers.SuspendExecution;
-import co.paralleluniverse.strands.SimpleConditionSynchronizer;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import com.lmax.disruptor.AlertException;
 import com.lmax.disruptor.EventProcessor;
 import com.lmax.disruptor.Sequence;
 import com.lmax.disruptor.WaitStrategy;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.SimpleConditionSynchronizer;
 
 /**
  * Blocking strategy that uses a lock and condition variable for {@link EventProcessor}s waiting on a barrier.
@@ -57,11 +53,13 @@ public final class StrandBlockingWaitStrategy implements WaitStrategy {
             try {
                 for (int i = 0; (availableSequence = cursorSequence.get()) < sequence; i++) {
                     barrier.checkAlert();
-                    processorNotifyCondition.await(i);
+                    processorNotifyCondition.await(i, 1, TimeUnit.NANOSECONDS);
+                    // Fiber.yield();
                 }
             } finally {
                 processorNotifyCondition.unregister(token);
             }
+
         }
 
         while ((availableSequence = dependentSequence.get()) < sequence) {
@@ -71,8 +69,12 @@ public final class StrandBlockingWaitStrategy implements WaitStrategy {
         return availableSequence;
     }
 
-    public long waitFor1(long sequence, Sequence cursorSequence, Sequence dependentSequence, com.lmax.disruptor.SequenceBarrier barrier, long timeout, TimeUnit unit)
-            throws AlertException, InterruptedException, SuspendExecution, TimeoutException {
+    public long waitFor1(long sequence,
+                         Sequence cursorSequence,
+                         Sequence dependentSequence,
+                         com.lmax.disruptor.SequenceBarrier barrier,
+                         long timeout,
+                         TimeUnit unit) throws AlertException, InterruptedException, SuspendExecution, TimeoutException {
         long availableSequence;
         if ((availableSequence = cursorSequence.get()) < sequence) {
             long left = unit.toNanos(timeout);
@@ -80,7 +82,7 @@ public final class StrandBlockingWaitStrategy implements WaitStrategy {
 
             Object token = processorNotifyCondition.register();
             try {
-                for (int i=0; (availableSequence = cursorSequence.get()) < sequence; i++) {
+                for (int i = 0; (availableSequence = cursorSequence.get()) < sequence; i++) {
                     barrier.checkAlert();
                     processorNotifyCondition.await(i, left, TimeUnit.NANOSECONDS);
                     left = deadline - System.nanoTime();
