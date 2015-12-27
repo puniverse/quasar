c468107384e11a3c987fb7aceafd7ec332bf27e5
circlespainter <fabio@paralleluniverse.co>
2015-12-27 19:23:42 +0200

    Add live instrumentation `MethodHandle` test

---
+++ quasar-core/src/jdk9test/java/co/paralleluniverse/fibers/instrument/auto/AutoSingleUninstrCallSiteMethodHandleTest.java
@@ -0,0 +1,97 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2015, Parallel Universe Software Co. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.fibers.instrument.auto;
+
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.fibers.Suspendable;
+import co.paralleluniverse.strands.SuspendableCallable;
+import org.junit.Test;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * @author circlespainter
+ */
+public class AutoSingleUninstrCallSiteMethodHandleTest {
+    static class F implements SuspendableCallable<Integer> {
+        @Override
+        public Integer run() throws SuspendExecution, InterruptedException {
+            final String s = "ciao";
+            System.err.println("Enter run(), calling m(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            final int ret;
+            try {
+                ret = (Integer) MethodHandles.lookup().unreflect(this.getClass().getMethod("m", String.class)).invoke(this, s);
+            } catch (final Throwable e) {
+                throw new RuntimeException(e);
+            }
+            System.err.println("Exit run(), called m(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            return ret;
+        }
+
+        // @Suspendable
+        public int m(String s) {
+            System.err.println("Enter m(" + s + "), calling m1(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            final int ret;
+            try {
+                ret = (Integer) MethodHandles.lookup().unreflect(this.getClass().getMethod("m1", String.class)).invoke(this, s);
+            } catch (final Throwable e) {
+                throw new RuntimeException(e);
+            }
+            System.err.println("Exit m(" + s + "), called m1(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            return ret;
+        }
+
+        @Suspendable
+        public int m1(String s) {
+            System.err.println("Enter m1(" + s + "), sleeping");
+            assertThat(s, equalTo("ciao"));
+            try {
+                Fiber.sleep(10);
+            } catch (final InterruptedException | SuspendExecution e) {
+                throw new RuntimeException(e);
+            }
+            System.err.println("Exit m1(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            return -1;
+        }
+    }
+
+    @Test public void test() {
+        final Fiber<Integer> f1 = new Fiber<>(new F()).start();
+        try {
+            assertThat(f1.get(), equalTo(-1));
+        } catch (final ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        final Fiber<Integer> f2 = new Fiber<>(new F()).start();
+        try {
+            assertThat(f2.get(), equalTo(-1));
+        } catch (final ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
