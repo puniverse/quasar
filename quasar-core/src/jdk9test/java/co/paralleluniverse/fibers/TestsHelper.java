993a9eef5057a91d49ee77648fc634d20f728c15
circlespainter <fabio@paralleluniverse.co>
2016-01-10 16:31:09 +0200

    Temporarily exclude main tests from jdk9 tests

---
+++ quasar-core/src/jdk9test/java/co/paralleluniverse/fibers/TestsHelper.java
@@ -0,0 +1,30 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2013-2014, Parallel Universe Software Co. All rights reserved.
+ * 
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *  
+ *   or (per the licensee's choosing)
+ *  
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.fibers;
+
+import co.paralleluniverse.fibers.Fiber;
+
+/**
+ *
+ * @author pron
+ */
+public class TestsHelper {
+    public static boolean exec(Fiber f) {
+        f.resetState();
+        return f.exec();
+    }
+    
+    private TestsHelper() {
+    }
+}
