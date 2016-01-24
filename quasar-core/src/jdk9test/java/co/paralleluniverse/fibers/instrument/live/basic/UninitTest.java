a7433d943117b266101dbb428594f8f8f08882f1
circlespainter <fabio@paralleluniverse.co>
2016-01-24 22:06:34 +0200

    Live instr.: test uninitialized locals

---
+++ quasar-core/src/jdk9test/java/co/paralleluniverse/fibers/instrument/live/basic/UninitTest.java
@@ -0,0 +1,93 @@
+/*
+ * Quasar: lightweight threads and actors for the JVM.
+ * Copyright (c) 2015, Parallel Universe Software Co. All rights reserved.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation
+ *
+ *   or (per the licensee's choosing)
+ *
+ * under the terms of the GNU Lesser General Public License version 3.0
+ * as published by the Free Software Foundation.
+ */
+package co.paralleluniverse.fibers.instrument.live.basic;
+
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.LiveInstrumentation;
+import co.paralleluniverse.fibers.instrument.live.LiveInstrumentationTest;
+import co.paralleluniverse.strands.SuspendableCallable;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * @author circlespainter
+ */
+//@Ignore
+public final class UninitTest extends LiveInstrumentationTest {
+    private static final class F implements SuspendableCallable<Double> {
+        @Override
+        // @Suspendable
+        public final Double run() throws InterruptedException {
+            byte v;
+            boolean b;
+            short k;
+            int i;
+            float f;
+            long l;
+            double d;
+            Object o;
+            final String s = "ciao";
+            System.err.println("Enter run(), calling m(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            final double ret = m(s);
+            System.err.println("Exit run(), called m(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            return ret;
+        }
+
+        // @Suspendable
+        private static double m(String s) throws InterruptedException {
+            byte v;
+            boolean b;
+            short k;
+            int i;
+            float f;
+            long l;
+            double d;
+            Object o;
+            System.err.println("Enter m(" + s + "), calling m1(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            Fiber.sleep(10);
+            System.err.println("Exit m(" + s + "), called m1(" + s + ")");
+            assertThat(s, equalTo("ciao"));
+            return -1.7;
+        }
+
+    }
+
+    @Test public final void test() {
+        final Fiber<Double> f1 = new Fiber<>(new F()).start();
+        try {
+            assertThat(f1.get(), equalTo(-1.7));
+        } catch (final ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        final Fiber<Double> f2 = new Fiber<>(new F()).start();
+        try {
+            assertThat(f2.get(), equalTo(-1.7));
+        } catch (final ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        assertThat(LiveInstrumentation.fetchRunCount(), equalTo(1L));
+    }
+}
