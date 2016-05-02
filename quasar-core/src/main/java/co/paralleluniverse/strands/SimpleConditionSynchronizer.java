7e588c7e26a432aa96cd051eb060f010ec5a0844
pron <ron.pressler@gmail.com>
2016-05-02 12:28:27 +0300

    -

--- quasar-core/src/main/java/co/paralleluniverse/strands/SimpleConditionSynchronizer.java
+++ quasar-core/src/main/java/co/paralleluniverse/strands/SimpleConditionSynchronizer.java
@@ -1,6 +1,6 @@
 /*
  * Quasar: lightweight threads and actors for the JVM.
- * Copyright (c) 2013-2014, Parallel Universe Software Co. All rights reserved.
+ * Copyright (c) 2013-2016, Parallel Universe Software Co. All rights reserved.
  * 
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
@@ -23,7 +23,7 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * @author pron
  */
 public class SimpleConditionSynchronizer extends ConditionSynchronizer implements Condition {
-    private final Queue<Strand> waiters = new ConcurrentLinkedQueue<Strand>();
+    private final Queue<Strand> waiters = new ConcurrentLinkedQueue<>();
 
     public SimpleConditionSynchronizer(Object owner) {
         super(owner);
