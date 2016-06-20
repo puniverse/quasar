ed2e58d5522253da80a41c71b423fb7fc3ced601
circlespainter <fabio@paralleluniverse.co>
2016-06-20 14:15:12 +0300

    :green_heart: Try to upgrade Travis env. and fix build

--- travis/pre_build.sh
+++ travis/pre_build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 start=$(date +%s)
 
-export JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
-echo ${JAVA_VER}
-
 if [[ ${JAVA_VER} == "18" ]]; then
     ls -al /usr/lib/jvm
     sudo apt-get -y remove --purge oracle-java8-installer*
@@ -11,14 +8,8 @@ if [[ ${JAVA_VER} == "18" ]]; then
     ls -al /usr/lib/jvm
     sudo apt-get -y update
     sudo apt-get -y install oracle-java8-installer
-    ls -al /usr/lib/jvm
     sudo ln -s /usr/lib/jvm/java-8-oracle /usr/lib/jvm/default-java
-    jdk_switcher use oraclejdk8
-    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-    echo ${JAVA_HOME}
-    java -version
-    javac -version
-    javah -version
+    ls -al /usr/lib/jvm
 fi
 
 end=$(date +%s)
