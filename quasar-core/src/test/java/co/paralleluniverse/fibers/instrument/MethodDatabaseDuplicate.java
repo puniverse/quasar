c7e0c08b7acac53228f08bda09c16d578ccb0849
Paul Hatcher <paul.hatcher@r3.com>
2020-06-01 12:11:44 +0100

    CORDA-3756 : Cherry picked from release/0.7 merge

---
+++ quasar-core/src/test/java/co/paralleluniverse/fibers/instrument/MethodDatabaseDuplicate.java
@@ -0,0 +1,39 @@
+package co.paralleluniverse.fibers.instrument;
+
+import org.objectweb.asm.Type;
+import co.paralleluniverse.fibers.Instrumented;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.Test;
+
+/**
+ * Test to check NPE in MethodDatabase.ClassEntry.equals.
+ * See https://github.com/puniverse/quasar/issues/286
+ * Duplicate DB entries are actually caused by a threading issue and a race in the instrumentation.
+ * This leads to duplicate class name in the database but with different ClassEntry objects.
+ * When testing ClassEntry objects for equality the equals operator is blowing up as the superName can be null.
+ * @author Paul Hatcher
+ */
+@Instrumented
+public class MethodDatabaseDuplicate {
+
+    @Test
+    public void testSuspend() throws IOException {
+        final String className = Type.getInternalName(MethodDatabaseDuplicate.class);
+        final QuasarInstrumentor instrumentor = new QuasarInstrumentor(false);
+        final MethodDatabase db = instrumentor.getMethodDatabase(MethodDatabaseDuplicate.class.getClassLoader());
+
+        // Create a DB entry for this class with null super.
+        db.getOrCreateClassEntry(className, null);
+
+        // Parse this class, just to exercise parsing and DB.
+        // Not strictly necessary for exception to occur. But good for coverage.
+        try (final InputStream in = MethodDatabaseDuplicate.class.getResourceAsStream("MethodDatabaseDuplicate.class")) {
+            instrumentor.instrumentClass(MethodDatabaseDuplicate.class.getClassLoader(), MethodDatabaseDuplicate.class.getName(), in, true);
+        }
+
+        // Now make it blow up by calling recordSuspendableMethods with a null super.
+        // This will find the duplicate but with a different (new) class entry.
+        db.recordSuspendableMethods(className, new MethodDatabase.ClassEntry(null));
+    }
+}
